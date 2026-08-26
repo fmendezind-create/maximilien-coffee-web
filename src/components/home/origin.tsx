413d485fea00f762d004b67440b2c2e2db5e4454
fmendezind-create <fmendez.ind@gmail.com>
2026-08-25 20:10:41 -0500

    Update origin.tsx

--- src/components/home/origin.tsx
+++ src/components/home/origin.tsx
@@ -13,7 +13,7 @@ export function Origin() {
     >
       <div
         className="absolute inset-0 bg-cover bg-center"
-        style={{ backgroundImage: "url(/images/origin-bg.svg)" }}
+        style={{ backgroundImage: "url(/images/Vista_Farm.webp)" }}
         aria-hidden="true"
       />
       <div
